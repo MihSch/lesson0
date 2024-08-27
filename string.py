eeb55f00b4f5fa176921159ac0c25c893a4140f2
MihSch <moco4ino@gmail.com>
2024-08-27 20:05:18 +0400

    Add files via upload

---
+++ string.py
@@ -0,0 +1,16 @@
+#name = "Miha"
+#print("Hello" + name) сложил строки
+#print("Hello" * 5) Пять раз хелоо
+#print(name[0]) Индексация пошла... рез=М ноль всегда начало
+#print(name[0:3]) рез=Mih
+#print(name[-1]) рез=a
+#print(name[0:3:2]) #рез=Mh :2 это шаг, пропустили i
+#print(name[:3]) # рез=Mih до третьего знака
+#print(name[2:]) # рез=ha со второго элемента по самый конец
+#print(name[::-1]) # рез= ahiM оставили только шаг
+a = "Топинамбур" #оиабр
+print (a [0])
+print (a [-1])
+print (a [5:])
+print (a [::-1])
+print (a [1::2])
